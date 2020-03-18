474e15da6c8320995c198dcbc45d6cc2f96a3b62
Veronica98 <alexa.veronica@yahoo.com>
2020-03-18 17:06:31 +0200

    Added enemies
    Am adaugat inamicii folosind un script cu stari : patrulare, atac si moarte.
    
    La patrulare am groundcheck si wallcheck care verifica daca am un perete in fata sau daca nu mai am nimic si pot sa cad de pe platforma/harta , functia flip care il intoarce la 180 de grade pentru a patrula si urmari  si la final moartea care depinde de health script (momentan este implementat in scriptul de enemy controller dar dupa ce testam si shootingul cu playerul am sa fac unul separat).

---
+++ Unity2DGame/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private enum State
+    {
+        Walking,
+        Knockback,
+        Dead
+    }
+    private State currentState;
+
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private float wallCheckDistance;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float knockbackDuration;
+
+    private int damageDirection;
+    private int facingDirection;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask whatIsGround;
+
+    private bool groundDetected;
+    private bool wallDetected;
+
+    private Rigidbody2D aliveRb;
+    private GameObject alive;
+
+
+    [SerializeField] private Vector2 knockbackSpeed;
+    private Vector2 movement;
+
+    private float currentHealth;
+    private float knockbackStartTime;
+
+    private void Start()
+    {
+        alive = transform.Find("Alive").gameObject;
+        aliveRb = alive.GetComponent<Rigidbody2D>();
+        facingDirection = 1;
+
+    }
+
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case State.Walking:
+                UpdateWalkingState();
+                break;
+            case State.Knockback:
+                UpdateKnockbackState();
+                break;
+            case State.Dead:
+                UpdateDeadState();
+                break;
+        }
+    }
+
+    //walking state
+    private void EnterWalkingState()
+    {
+
+    }
+
+    private void UpdateWalkingState() 
+    {
+        groundDetected = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+        wallDetected = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
+
+        if (!groundDetected || wallDetected)
+        {
+            Flip();
+        }
+        else
+        {
+            movement.Set(movementSpeed * facingDirection, aliveRb.velocity.y);
+            aliveRb.velocity = movement;
+        }
+    }
+
+    private void ExitWalkingState()
+    {
+
+    }
+
+    //knockback state
+
+    private void EnterKnockbackState()
+    {
+        knockbackStartTime = Time.time;
+        movement.Set(knockbackSpeed.x * damageDirection, knockbackSpeed.y);
+        aliveRb.velocity = movement;
+    }
+
+    private void UpdateKnockbackState()
+    {
+        if (Time.time >= knockbackStartTime + knockbackDuration)
+        {
+            SwitchState(State.Walking);
+        }
+    }
+
+    private void ExitKnockbackState()
+    {
+
+    }
+
+    //dead state
+
+    private void EnterDeadState()
+    {
+        Destroy(gameObject);
+    }
+
+    private void UpdateDeadState()
+    {
+
+    }
+
+    private void ExitDeadState()
+    {
+
+    }
+
+    private void Damage(float[] attackDetails)
+    {
+        currentHealth -= attackDetails[0];
+
+        if(attackDetails[1] > alive.transform.position.x)
+        {
+            damageDirection = -1;
+        }
+        else
+        {
+            damageDirection = 1;
+        }
+
+        if(currentHealth > 0.0f)
+        {
+            SwitchState(State.Knockback);
+        }
+        else if(currentHealth <= 0.0f)
+        {
+            SwitchState(State.Dead);
+        }
+    }
+
+    private void Flip()
+    {
+        facingDirection *= -1;
+        alive.transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+
+    private void SwitchState(State state)
+    {
+        switch (currentState)
+        {
+            case State.Walking:
+                ExitWalkingState();
+                break;
+            case State.Knockback:
+                ExitKnockbackState();
+                break;
+            case State.Dead:
+                ExitDeadState();
+                break;
+
+        }
+
+        switch (state)
+        {
+            case State.Walking:
+                EnterWalkingState();
+                break;
+            case State.Knockback:
+                EnterKnockbackState();
+                break;
+            case State.Dead:
+                EnterDeadState();
+                break;
+
+        }
+        currentState = state;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(wallCheck.position, new Vector2(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+        Gizmos.DrawLine(groundCheck.position, new Vector2(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+    }
+    
+}
