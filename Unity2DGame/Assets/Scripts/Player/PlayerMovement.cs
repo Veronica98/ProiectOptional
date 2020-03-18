fae3e08b041fbc7616007c96f238154ffd751cb2
Ifrim Denis <ifrimdenis619@gmail.com>
2020-03-18 15:40:43 +0200

    Refactoring Code
    Am schimbat putin logica movement-ului din scriptul playerului pentru a fi mai usor de inteles codul.
    
    Am creat prefaburi pentru platforma mica, mare si block de grass pentru a fi mai usor pe viitor crearea hartilor.
    
    Am adaugat posibilitatea de double jump la player.
    
    Am adaugat un physics material cu friction 0 pentru majoritatea platformelor pentru ca jucatorul sa nu ramana prins de pereti.
    
    Am organizat scripturile, materialele si altele in foldere pentru vizibilitate mai buna.

---
+++ Unity2DGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+    private int extraJumps;
+    public int extraJumpsValue; 
+
+    private Rigidbody2D rb;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxisRaw    ("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+
+        if((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && extraJumps == 0 && isGrounded ==true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+}
