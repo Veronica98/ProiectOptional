81b2fa13530b8a251034e06c1f36d086d95055dd
Ifrim Denis <ifrimdenis619@gmail.com>
2020-03-18 20:00:43 +0200

    Player attack
    Am implementat attackul ranged care consta in tragerea proiectilelor. Proiectilele se trag doar la un anumit interval si nu se pot spama.
    
    Am implementat attackul melee.
    
    Am implementat posibilitatea schimbarii dintre atacul melee si atacul rannged prin apasarea tastei Q.

---
+++ Unity2DGame/Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody2D rb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+        Destroy(gameObject, 3f);
+    }
+
+    void OnTriggerEnter2D (Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        if (hitInfo.name != "Player")
+        {
+
+            Destroy(gameObject);
+
+        }
+    }
+
+   
+}
---
+++ Unity2DGame/Assets/Scripts/Player/WeaponSwap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwap : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<PlayerMeleeAtack>().enabled && !player.GetComponent<Weapon>().enabled)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                player.GetComponent<Weapon>().enabled = true;
+                player.GetComponent<PlayerMeleeAtack>().enabled = false;
+            }
+        }
+        else if (!player.GetComponent<PlayerMeleeAtack>().enabled && player.GetComponent<Weapon>().enabled)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                player.GetComponent<Weapon>().enabled = false;
+                player.GetComponent<PlayerMeleeAtack>().enabled = true;
+            }
+        }
+    }
+}
