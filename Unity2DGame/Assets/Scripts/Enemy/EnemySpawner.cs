82606a32a90a882b2b45989ef7a0aadf6495e66d
Ifrim Denis <ifrimdenis619@gmail.com>
2020-04-05 01:16:16 +0300

    First version of spawners
    Am implementat prima versiune de spawner.
    
    Va trebui sa mai fie implementat o versiune pentru fiecare mob in parte in functie de unde se va afla spawner-ul.
    
    Trebuie modificat pentru cand va exista notiunea de dificultate ca spawn rate-ul sa fie in functie de dificultate.

---
+++ Unity2DGame/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] enemies;
+    private float randX;
+    private Vector2 whereToSpawn;
+    private float spawnRate = 2f;
+    private float nextSpawn;
+
+
+
+    private void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randX = transform.position.x + Random.Range(-2f, 2f);
+            whereToSpawn = new Vector2(randX, transform.position.y);
+            Instantiate(enemies[Random.Range(0, enemies.Length)], whereToSpawn, Quaternion.identity);
+        }
+    }
+
+
+
+}
