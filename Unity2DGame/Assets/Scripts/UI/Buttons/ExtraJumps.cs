9fe7ebe6cd1862ba077681102e63e8ce5f27e6c6
Veronica98 <alexa.veronica@yahoo.com>
2020-04-13 22:04:05 +0300

    Am adaugat restul staturilor in shop
    Ele se upgradeaza daca apesi pe butonul respectiv statului ce vrei sa il upgradezi.
    
    Am facut putin balancing la ele,astfel staturile care ar fi mai OP costa mai mult si au o limita astfel incat daca o ating ele nu mai pot fi upgradate.

---
+++ Unity2DGame/Assets/Scripts/UI/Buttons/ExtraJumps.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExtraJumps : MonoBehaviour
+{
+    private int score;
+    [SerializeField] private int cost = 600;
+    [SerializeField] private int extraJumpsChange = 1;
+    [SerializeField] private Text extraJumpsText;
+    private float limitExtraJumps = 5;
+
+
+
+    public void upgradeExtraJumps()
+    {
+        score = GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreSkillTree>().getScore();
+        if (score - cost > 0.00001)
+        {
+
+            string saveString = SaveSystem.Load();
+
+            if (saveString != null)
+            {
+
+                SaveObject saveObject = JsonUtility.FromJson<SaveObject>(saveString);
+
+                if (saveObject.extraJumps + extraJumpsChange <= limitExtraJumps)
+                {
+                    score -= cost;
+                    saveObject.extraJumps += extraJumpsChange;
+                    saveObject.score -= cost;
+                    string json = JsonUtility.ToJson(saveObject);
+                    SaveSystem.SaveAfterShop(json);
+                    extraJumpsText.text = "Extra Jumps: " + saveObject.extraJumps.ToString();
+                    GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreSkillTree>().setScore(-cost);
+                }
+
+            }
+
+
+        }
+    }
+
+
+    public void Test()
+    {
+        Debug.Log("Merge on click");
+    }
+
+
+
+    private class SaveObject
+    {
+        public float maxHealth = 50;
+        public int extraJumps = 1; //
+        public float movementSpeed = 10;
+        public float meleeDamage = 20;
+        public float rangeDamage = 20;
+        public float fireRate = 0.5f; // 0.05
+        public float critChance = 10f;
+        public float critDamage = 50f;
+        public int score = 0;
+    }
+}
---
+++ Unity2DGame/Assets/Scripts/UI/Buttons/MovementSpeed.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovementSpeed : MonoBehaviour
+{
+    private int score;
+    [SerializeField] private int cost = 100;
+    [SerializeField] private float movementSpeedChange = 1;
+    [SerializeField] private Text movementSpeedText;
+    private float limitMovementSpeed = 50;
+
+
+
+    public void upgradeMovementSpeed()
+    {
+        score = GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreSkillTree>().getScore();
+        if (score - cost > 0.00001 )
+        {
+            
+            string saveString = SaveSystem.Load();
+
+            if (saveString != null)
+            {
+                
+                SaveObject saveObject = JsonUtility.FromJson<SaveObject>(saveString);
+
+                if(saveObject.movementSpeed + movementSpeedChange <= limitMovementSpeed)
+                {
+                    score -= cost;
+                    saveObject.movementSpeed += movementSpeedChange;
+                    saveObject.score -= cost;
+                    string json = JsonUtility.ToJson(saveObject);
+                    SaveSystem.SaveAfterShop(json);
+                    movementSpeedText.text = "Movement Speed: " + saveObject.movementSpeed.ToString();
+                    GameObject.FindGameObjectWithTag("Score").GetComponent<ScoreSkillTree>().setScore(-cost);
+                }
+                
+            }
+
+
+        }
+    }
+
+
+    public void Test()
+    {
+        Debug.Log("Merge on click");
+    }
+
+
+
+    private class SaveObject
+    {
+        public float maxHealth = 50;
+        public int extraJumps = 1; //
+        public float movementSpeed = 10;
+        public float meleeDamage = 20;
+        public float rangeDamage = 20;
+        public float fireRate = 0.5f; // 0.05
+        public float critChance = 10f; 
+        public float critDamage = 50f;
+        public int score = 0;
+    }
+}
