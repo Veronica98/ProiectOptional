2ad0538e430f99dbaf8dbcea80bc689eadb02eb7
StefanColis <57714517+StefanColis@users.noreply.github.com>
2020-04-24 16:10:03 +0300

    ParticleSystem
    It sucks. But it works. Mostly. Still need better particles and extra cases for the ParticleControl script.

---
+++ Unity2DGame/Assets/Scripts/Player/ParticleControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleControl : MonoBehaviour
+{
+    public ParticleSystem part;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            // Should load different Particle for every type of ColliderTag;
+            part.Play();
+        }
+    }
+}
