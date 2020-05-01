f732211d51797d79698465d6d90830bcfb2bf1bd
Ifrim Denis <ifrimdenis619@gmail.com>
2020-05-01 21:08:43 +0300

    Added new character

--- Unity2DGame/Assets/Scripts/Chest/ChestScript.cs
+++ Unity2DGame/Assets/Scripts/Chest/ChestScript.cs
@@ -8,7 +8,8 @@ public class ChestScript : MonoBehaviour
     public GameObject[] objects;
     public Transform spawnPoint;
     private bool chestOpened = false;
-
+    [SerializeField] private Animator animator;
+    private bool animationEnd = false;
 
 
 
@@ -23,10 +24,23 @@ public class ChestScript : MonoBehaviour
         }
     }
 
+
+    void animationStatusChange(string animationStatusChanged)
+    {
+        if (animationStatusChanged == "true")
+        {
+            animationEnd = true;
+        }
+    }
+
+
+
     void OpenChest()
     {
         
+
         chestOpened = true;
+        animator.SetBool("Open", true);
         int random = Random.Range(0, 100);
 
         if(random == 0 )
