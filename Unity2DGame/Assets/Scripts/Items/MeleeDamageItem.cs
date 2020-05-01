3c520a13be955e89fae9f649401c1f79168bad43
Ifrim Denis <ifrimdenis619@gmail.com>
2020-05-01 19:51:48 +0300

    Raw implementation of custom loot table with drop chance

--- Unity2DGame/Assets/Scripts/Items/MeleeDamageItem.cs
+++ Unity2DGame/Assets/Scripts/Items/MeleeDamageItem.cs
@@ -13,12 +13,12 @@ public class MeleeDamageItem : MonoBehaviour
     }
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             player.GetComponent<PlayerStats>().setMeleeDamage(meleeDamageChange); // Daca exista coliziune cu player-ul se apeleaza functia din PlayerStats pentru stat-ul respectiv si apoi se distruge obiectul instant
-            Destroy(gameObject);
+            Destroy(gameObject, 2f);
         }
     }
 }
